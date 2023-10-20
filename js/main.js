ad4ff781027a0ea8b7ac91354bf78e8173fcbe63
puescodigo <clubpuescodigo@gmail.com>
2023-10-20 01:16:24 -0500

    renovando codigo para navbar, hero y about.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,53 @@
+// GSAP
+
+//   animation for title
+  let tl = gsap.timeline({defaults: {ease: "SlowMo.easeOut"}})
+        tl.to(".espa", {
+        y: "0%",
+        duration: 0.8,
+        stagger: 0.2,
+    });
 
 
 
-// const myText = new SplitType('.hero__title__header')
 
-//         gsap.to('.char', {
-//             y: 0,
-//             stagger: 0.05,
-//             delay: 0.2,
-//             duration: .1
-//         })
+
+
+
+
+
+
+
+
+
+
+/* -- Logo effect -- */
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".nav__logo").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 6;
+  }, 25);
+}
